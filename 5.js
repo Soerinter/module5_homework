a004c37a8b64ee12d5e5edbcd06d9c179f1ce627
Soerinter <102628223+Soerinter@users.noreply.github.com>
2023-12-15 22:28:33 +0300

    5.js

---
+++ 5.js
@@ -0,0 +1,13 @@
+5
+
+const footballClubs = ['Интер', 'Реал', 'МЮ'];
+  
+console.log(footballClubs.length)
+
+footballClubs.forEach(function(fruit, index) {
+
+
+console.log(`Клуб по индексом ${index}: ${fruit}`);
+
+
+});
