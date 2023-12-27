e1e870f743081e585c9be75d1d74bbf2bbd2cec1
Soerinter <102628223+Soerinter@users.noreply.github.com>
2023-12-27 19:45:53 +0300

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,9 +1,11 @@
-const Even = (K) => K % 2 === 0;
-
-const Odd = (K) => K % 2 != 0;
-
 const arr = [1,2,3,4,5,6,7,8,9,10,11];
- 
+
+
+const Even = (i) => i % 2 === 0;
+
+const Odd = (i) => i % 2 != 0;
+
+
 const evenAmount = arr.filter(Even).length;
 
 const oddAmount = arr.filter(Odd).length;
