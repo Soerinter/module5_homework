ab97ce935689e9901204e44bb7a6db940eb551bf
Soerinter <102628223+Soerinter@users.noreply.github.com>
2023-12-27 14:43:22 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,7 +1,7 @@
- var arr = [5, 5, 5, 5];
+ let arr = [5, 5, 5, 5];
 
  function equal (a) {
-   for (var b = 0; b < a.length; b++)
+   for (let b = 0; b < a.length; b++)
      if (a[b] !== a[0]) return !1;
    return !0
  };
